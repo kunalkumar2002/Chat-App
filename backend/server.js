b9f528aa7412c44f18cd9461dcb88b27415264a3
Kunal Kumar <kunalkumar7091679623@gmail.com>
2024-02-18 20:24:27 +0530

    authentication of user and incrypting the password

--- backend/server.js
+++ backend/server.js
@@ -3,28 +3,21 @@ import dotenv from "dotenv";
 import { chats } from "./data/data.js";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const server = express();
 dotenv.config();
 connectDB();
+ 
 const PORT = process.env.PORT || 5000;
 server.use(cors());
+
+server.use(express.json());
+
 server.get("/", (req, res) => {
   return res.send("welcome to new chat APP");
 });
-
-// server.get("/api/chat", (req, res) => {
-//   return res.send(chats);
-// });
-
-// //in request section there is params object which have id property
-// server.get("/api/chat/:id", (req, res) => {
-//   //console.log("id :- " , req.params.id);
-//   const requiredChat = chats.find((c) => c._id === req.params.id);
-//   return res.send(requiredChat);
-// });
-
-
+server.use("/api/user", userRoutes);
 
 server.listen(PORT, () => {
   console.log(`server is up and and runing on ${PORT} `);
