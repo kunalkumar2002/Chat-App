d1982406c3744e2c3ced3f084b067591dcdf914f
Kunal Kumar <kunalkumar7091679623@gmail.com>
2024-02-23 14:45:25 +0530

    complitng chats route

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { accessChat } from "../controllers/chatController.js";
+import { accessChat, fetchChats,creatGroupChat,renameGroup,addToGroup,removeFromGroup } from "../controllers/chatController.js";
 
 const router = express.Router();
 
-router.route('/').post(protect, accessChat);
-//router.route("/").get(protect, fetchChats);
-//router.route("/").post(protect,creatGroupChat);
-//router.route("/groupremove").put(protect,removeFromGroup);
-//router.route("/groupadd").put(protect, addToGroup);
+router.route("/").post(protect, accessChat);
+router.route("/").get(protect, fetchChats);
+router.route("/group").post(protect, creatGroupChat);
+router.route("/rename").put(protect,renameGroup);
+router.route("/groupremove").put(protect,removeFromGroup);
+router.route("/groupadd").put(protect, addToGroup);
 
-export default router
+export default router;
